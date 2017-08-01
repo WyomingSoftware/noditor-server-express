f00dc1c2852765e839d7bc2e3d935cdbfb9202e1
wkande <warren@wyomingsoftware.com>
2017-08-01 13:52:56 -0600

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var noditor = require('noditor');
 var express = require('express');
+
+// Express
 var app = express();
 
-
+// Add the endpoint needed by the Noditor Mobile App
 app.get('/noditor/:passcode/:command', noditor.commands);
 
+// Start Express
 app.listen(8080, function () {
     console.log('Server > '+app.name+' started @'+new Date());
     var options = {"quiet":false};
